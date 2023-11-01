66fbb831587cb544ca30209449af3280b1a5443e
BrandonJE <khaos1004@naver.com>
2023-11-01 18:03:38 +0900

    first commit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+export default function FristPost() {
+    return <h1>First Posts</h1>
+  }
+
+// export default Keroro() ; {
+//     return(
+//         <div>
+//         <h1>gee</h1>
+//         </div>
+//     )
+// }
